6dc0070717a2aa702e5ddb7469a04cd1509862cb
Philip Harrison <philip@mailharrison.com>
2021-02-26 10:44:41 +0000

    Delete v2 helpers

--- bundler/lib/dependabot/bundler/helpers.rb
+++ bundler/lib/dependabot/bundler/helpers.rb
@@ -3,11 +3,9 @@
 module Dependabot
   module Bundler
     module Helpers
-      def self.bundler_version(lockfile)
-        return "v1" unless lockfile
-
-        # return "v2" if lockfile.content.match?(/BUNDLED WITH\s+2/m)
-
+      # TODO: Add support for bundler v2
+      # return "v2" if lockfile.content.match?(/BUNDLED WITH\s+2/m)
+      def self.bundler_version(_lockfile)
         "v1"
       end
     end
